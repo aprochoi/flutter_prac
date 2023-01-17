44f784e08526d844c8ac951168738aff9d190fb2
aprochoi <ym24319577@gmail.com>
2023-01-17 20:24:20 +0900

    flutter todoList

---
+++ flutter_todo_list/lib/screens/login_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/screens/list_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  Future setLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isLogin', true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('로그인'),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          width: MediaQuery.of(context).size.width * 0.85,
+          child: ElevatedButton(
+            onPressed: () {
+              setLogin().then((_) {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => ListScreen())
+                );
+              });
+            },
+            child: const Text('로그인'),
+          ),
+        )
+      ),
+    );
+  }
+}
+
