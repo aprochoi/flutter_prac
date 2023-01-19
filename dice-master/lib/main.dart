44519cda45a57aa46f63ea12e619f0b8df60967b
aprochoi <ym24319577@gmail.com>
2023-01-20 08:59:54 +0900

    Value Key Test

--- dice-master/lib/main.dart
+++ dice-master/lib/main.dart
@@ -20,7 +20,7 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
-
+  bool emailField = true;
   TextEditingController controller = TextEditingController();
   TextEditingController controller2 = TextEditingController();
 
@@ -28,25 +28,25 @@ class _LogInState extends State<LogIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Log in'),
+        title: const Text('Log in'),
         backgroundColor: Colors.redAccent,
         centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        leading: IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search), onPressed: () {})
+          IconButton(icon: const Icon(Icons.search), onPressed: () {})
         ],
       ),
       body: Builder(
-        builder:(context) {
+        builder: (context) {
           return GestureDetector(
-            onTap: (){
+            onTap: () {
               FocusScope.of(context).unfocus();
             },
             child: SingleChildScrollView(
               child: Column(
                 children: <Widget>[
-                  Padding(padding: EdgeInsets.only(top: 50)),
-                  Center(
+                  const Padding(padding: EdgeInsets.only(top: 50)),
+                  const Center(
                     child: Image(
                       image: AssetImage('image/chef.gif'),
                       width: 170.0,
@@ -57,55 +57,48 @@ class _LogInState extends State<LogIn> {
                       child: Theme(
                           data: ThemeData(
                               primaryColor: Colors.teal,
-                              inputDecorationTheme: InputDecorationTheme(
-                                  labelStyle:
-                                  TextStyle(color: Colors.teal, fontSize: 15.0))),
+                              inputDecorationTheme: const InputDecorationTheme(
+                                  labelStyle: TextStyle(
+                                      color: Colors.teal, fontSize: 15.0))),
                           child: Container(
-                            padding: EdgeInsets.all(40.0),
+                            padding: const EdgeInsets.all(40.0),
                             child: Column(
                               children: <Widget>[
-                                TextField(
-                                  controller: controller,
-                                  decoration:
-                                  InputDecoration(labelText: 'Enter "image"'),
-                                  keyboardType: TextInputType.emailAddress,
-                                ),
-                                TextField(
-                                  controller: controller2,
-                                  decoration:
-                                  InputDecoration(labelText: 'Enter Password'),
+                                if (emailField)
+                                  const TextField(
+                                    key: ValueKey(1),
+                                    // controller: controller,
+                                    decoration: InputDecoration(
+                                        labelText: 'Enter "dice"'),
+                                    keyboardType: TextInputType.emailAddress,
+                                  ),
+                                const TextField(
+                                  key: ValueKey(2),
+                                  // controller: controller2,
+                                  decoration: InputDecoration(
+                                      labelText: 'Enter Password'),
                                   keyboardType: TextInputType.text,
-                                  obscureText: true,
+                                  obscureText: false,
                                 ),
-                                SizedBox(
+                                const SizedBox(
                                   height: 40.0,
                                 ),
                                 ButtonTheme(
                                     minWidth: 100.0,
                                     height: 50.0,
-                                    child: RaisedButton(
-                                        color: Colors.orangeAccent,
-                                        child: Icon(
-                                          Icons.arrow_forward,
+                                    child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            primary: Colors.purple,
+                                            minimumSize: const Size(150, 50)),
+                                        child: const Icon(
+                                          Icons.visibility_off,
                                           color: Colors.white,
                                           size: 35.0,
                                         ),
                                         onPressed: () {
-
-                                          if(controller.text == 'image' && controller2.text == '1234'){
-                                            Navigator.push(context,
-                                                MaterialPageRoute(builder: (BuildContext context)=>Dice()));
-                                          }else if(controller.text == 'image' && controller2.text != '1234'){
-                                            showSnackBar2(context);
-
-                                          }else if(controller.text != 'image' && controller2.text == '1234'){
-                                            showSnackBar3(context);
-
-                                          }else {
-                                            showSnackBar(context);
-
-                                          }
-
+                                          setState(() {
+                                            emailField = false;
+                                          });
                                         })),
                               ],
                             ),
@@ -120,38 +113,35 @@ class _LogInState extends State<LogIn> {
   }
 }
 
-void showSnackBar(BuildContext context){
-
-  Scaffold.of(context).showSnackBar(
-      SnackBar(content:
-      Text('로그인 정보를 다시 확인하세요',
-        textAlign: TextAlign.center,),
-        duration: Duration(seconds: 2),
-        backgroundColor: Colors.blue,
-      )
-  );
+void showSnackBar(BuildContext context) {
+  Scaffold.of(context).showSnackBar(const SnackBar(
+    content: Text(
+      '로그인 정보를 다시 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
 
-void showSnackBar2(BuildContext context){
-
-  Scaffold.of(context).showSnackBar(
-      SnackBar(content:
-      Text('비밀번호가 일치하지 않습니다',
-        textAlign: TextAlign.center,),
-        duration: Duration(seconds: 2),
-        backgroundColor: Colors.blue,
-      )
-  );
+void showSnackBar2(BuildContext context) {
+  Scaffold.of(context).showSnackBar(const SnackBar(
+    content: Text(
+      '비밀번호가 일치하지 않습니다',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
 
-void showSnackBar3(BuildContext context){
-
-  Scaffold.of(context).showSnackBar(
-      SnackBar(content:
-      Text('dice의 철자를 확인하세요',
-        textAlign: TextAlign.center,),
-        duration: Duration(seconds: 2),
-        backgroundColor: Colors.blue,
-      )
-  );
+void showSnackBar3(BuildContext context) {
+  Scaffold.of(context).showSnackBar(const SnackBar(
+    content: Text(
+      'dice의 철자를 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
