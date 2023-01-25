86ff54791663233fcd4cd4afc750e4992919cd6f
aprochoi <ym24319577@gmail.com>
2023-01-25 16:45:44 +0900

    Header 작성

--- ui_test/lib/main.dart
+++ ui_test/lib/main.dart
@@ -26,8 +26,43 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Hello Flutter'),
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hey, Selena',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Text(
+                      'Welcomde back',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
