ed113965533daa14efc58e688886ac352a676db7
aprochoi <ym24319577@gmail.com>
2023-01-31 21:05:40 +0900

    UI

--- ui_test/lib/main.dart
+++ ui_test/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_test/widgets/Button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyPage extends StatelessWidget {
       backgroundColor: const Color(0xFF181818),
       body: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 20,
+          horizontal: 10,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -65,7 +66,7 @@ class MyPage extends StatelessWidget {
               ],
             ),
             const SizedBox(
-              height: 120,
+              height: 80,
             ),
             Text(
               'Total balance',
@@ -90,49 +91,110 @@ class MyPage extends StatelessWidget {
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Button(
+                  text: 'Transfer',
+                  bgColor: Color(0xFFF2B33A),
+                  textColor: Colors.black,
+                ),
+                Button(
+                  text: 'Requeset',
+                  bgColor: Color(0xFF1F2123),
+                  textColor: Colors.white,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                      color: const Color(
-                        0xFFF2B33A,
-                      ),
-                      borderRadius: BorderRadius.circular(
-                        45,
-                      )),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 20,
-                      horizontal: 50,
-                    ),
-                    child: Text(
-                      'Transfer',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
+                const Text(
+                  'Wallets',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: const Color(0xFF1F2123),
-                      borderRadius: BorderRadius.circular(
-                        45,
-                      )),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 20,
-                      horizontal: 50,
+                Text(
+                  'View All',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                color: const Color(0xFF1F2123),
+                borderRadius: BorderRadius.circular(
+                  25,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Euro',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              '6, 428',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              'EUR',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    child: Text(
-                      'Request',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
+                    Transform.scale(
+                      scale: 2.2,
+                      child: Transform.translate(
+                        offset: const Offset(-5, 12),
+                        child: const Icon(
+                          Icons.euro_rounded,
+                          color: Colors.white,
+                          size: 88,
+                        ),
                       ),
                     ),
-                  ),
-                )
-              ],
+                  ],
+                ),
+              ),
             ),
           ],
         ),
