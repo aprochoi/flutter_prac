8b6951c70e083edeaf8741edd9a6edaf7f1a4b46
aprochoi <ym24319577@gmail.com>
2023-02-03 12:29:32 +0900

    API GET 요청 테스트

--- webtoon_app/lib/main.dart
+++ webtoon_app/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon_app/screens/home_screen.dart';
+import 'package:webtoon_app/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
