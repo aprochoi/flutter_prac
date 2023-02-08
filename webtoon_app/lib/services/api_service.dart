8c6f482b581f052093a89589a7344f400c745c9d
aprochoi <ym24319577@gmail.com>
2023-02-08 11:38:11 +0900

    DetailModel, EpisodesModel API 서비스 구현

--- webtoon_app/lib/services/api_service.dart
+++ webtoon_app/lib/services/api_service.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:webtoon_app/models/webotoon._model.dart';
+import 'package:webtoon_app/models/webtoon_detail_model.dart';
+import 'package:webtoon_app/models/webtoon_episode_model.dart';
 
 class ApiService {
   static const String baseUrl =
@@ -20,6 +22,36 @@ class ApiService {
       }
       return webtoonInstances;
     }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonByID(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesByID(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesList = [];
+
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesList.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesList;
+    }
+
     throw Error();
   }
 }
